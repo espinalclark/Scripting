a0302819c6f6a9d675a109b5a7b4b2ed58fe888a
espinalclark <7clarkespinal@gmail.com>
2025-11-16 02:10:03 -0500

    limpiando

--- Python/Multi-Thread/main.py
+++
@@ -1,6 +0,0 @@
-# main.py
-from login import iniciar_login
-
-if __name__ == "__main__":
-    iniciar_login()
-
--- Python/Multi-Thread/utils.py
+++
@@ -1,6 +0,0 @@
-# utils.py
-import re
-
-def validar_url(url):
-    return re.match(r'https?://', url) is not None
-
